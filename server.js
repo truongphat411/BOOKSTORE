70c4ee34df4d0c0d3ffb6dfef800eac100b72f65
truongphat411 <nguyenmaitruongphat@gmail.com>
2022-04-13 03:05:36 +0700

    push len nhanh PhatDev

--- server.js
+++ server.js
@@ -25,5 +25,7 @@ app.use('/api/filter', filterRoutes);
 
 connectDB();
 
+////////////////////////////
+
 const port = process.env.port || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
