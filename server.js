734f2531883a47aac600f04a92a69ea109ec7ceb
truongphat411 <nguyenmaitruongphat@gmail.com>
2022-04-13 03:35:09 +0700

    master commit

--- server.js
+++ server.js
@@ -25,12 +25,6 @@ app.use('/api/filter', filterRoutes);
 
 connectDB();
 
-<<<<<<< HEAD
-=======
-/////////////////////////////////////////
-/////////////////////////////////////////
-
->>>>>>> PhatDev
 
 const port = process.env.port || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
